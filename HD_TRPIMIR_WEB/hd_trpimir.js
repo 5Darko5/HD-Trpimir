aa50ce67eefdd736afe1c23d9def05534dfab30a
PREDATOR\Darko <darko.ivankovic@morh.hr>
2025-02-24 22:09:18 +0100

    240225 vecer
    Co-authored-by: elegAntee <elegAntee@users.noreply.github.com>

---
+++ HD_TRPIMIR_WEB/hd_trpimir.js
@@ -0,0 +1,17 @@
+const tipkaPocetna = document.getElementById("tipka-pocetna")
+const tipkaODrustvu = document.getElementById("tipka-o-društvu")
+const tipkaKlape = document.getElementById("tipka-klape")
+const tipkaPublikacije = document.getElementById("tipka-publikacije")
+const tipkaGalerija = document.getElementById("tipka-galerija")
+const tipkaStatut = document.getElementById("tipka-statut")
+
+function openLink(unesiLink){
+    window.open(unesiLink, "_self")
+}
+
+//tipkaPocetna.addEventListener("click", function() {openLink("")});
+//tipkaODrustvu.addEventListener("click", function() {openLink("")});
+//tipkaKlape.addEventListener("click", function() {openLink("")});
+//tipkaPublikacije.addEventListener("click", function() {openLink("")});
+//tipkaGalerija.addEventListener("click", function() {openLink("")});
+//tipkaStatut.addEventListener("click", function() {openLink("")});
