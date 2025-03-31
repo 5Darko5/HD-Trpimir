49f08286364bb128348ae5ecdac6adcb0db102de
PREDATOR\Darko <darko.ivankovic@morh.hr>
2025-03-31 23:55:07 +0200

    ajde

--- HD_TRPIMIR_WEB/hd_trpimir.js
+++ HD_TRPIMIR_WEB/hd_trpimir.js
@@ -4,14 +4,16 @@ const tipkaKlape = document.getElementById("tipka-klape")
 const tipkaPublikacije = document.getElementById("tipka-publikacije")
 const tipkaGalerija = document.getElementById("tipka-galerija")
 const tipkaStatut = document.getElementById("tipka-statut")
+const tipkaClanovi = document.getElementById("tipka-clanovi")
 
 function openLink(unesiLink){
     window.open(unesiLink, "_self")
 }
 
-//tipkaPocetna.addEventListener("click", function() {openLink("")});
-//tipkaODrustvu.addEventListener("click", function() {openLink("")});
-//tipkaKlape.addEventListener("click", function() {openLink("")});
-//tipkaPublikacije.addEventListener("click", function() {openLink("")});
-//tipkaGalerija.addEventListener("click", function() {openLink("")});
-//tipkaStatut.addEventListener("click", function() {openLink("")});
+tipkaPocetna.addEventListener("click", function() {openLink("1_hd_trpimir.html")});
+tipkaODrustvu.addEventListener("click", function() {openLink("2_o-drustvu.html")});
+tipkaKlape.addEventListener("click", function() {openLink("3_klape.html")});
+tipkaPublikacije.addEventListener("click", function() {openLink("4_publikacije.html")});
+tipkaGalerija.addEventListener("click", function() {openLink("5_galerija.html")});
+tipkaStatut.addEventListener("click", function() {openLink("6_statut.html")});
+tipkaClanovi.addEventListener("click", function() {openLink("7_clanovi.html")});
